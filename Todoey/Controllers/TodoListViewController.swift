f3b9f5138e74de85c04d97ee2d7148b9578e9be6
Prince <Princemumbai@gmail.com>
2019-03-11 10:46:39 +1100

    Cleaned code - data stored using own plist by encoding and decoding

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -10,43 +10,13 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-//    var itemArray = ["Find Mike","Buy Eggs","Destroy Demogorgon"]
     var itemArray = [Item]()
     let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
-//    let defaults = UserDefaults.standard
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
- 
-        print (dataFilePath)
-        
-        let newItem = Item()
-        newItem.title = "Find Mike"
-        itemArray.append(newItem)
-        
-        let newItem1 = Item()
-        newItem1.title = "Buy Eggs"
-        itemArray.append(newItem1)
-
-        let newItem2 = Item()
-        newItem2.title = "Destroy Demogorgon"
-        itemArray.append(newItem2)
-
-        let newItem3 = Item()
-        newItem3.title = "Complete App learning"
-        itemArray.append(newItem3)
-
-        // Do any additional setup after loading the view, typically from a nib.
-//        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
-//            itemArray = items
-        
         loadItems()
-//        }
-        
     }
-
 
     //MARK - Tableview Datasource Methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -56,54 +26,30 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "todoItemCell", for: indexPath)
         cell.textLabel?.text = itemArray[indexPath.row].title
-        
         cell.accessoryType = itemArray[indexPath.row].done ? .checkmark : .none
-        
-//        if itemArray[indexPath.row].done == true {
-//            cell.accessoryType = .checkmark
-//        } else{
-//            cell.accessoryType = .none
-//        }
-        
         return cell
         
     }
     
     //MARK - TableView Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-       // print(itemArray[indexPath.row])
-        
+
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
-        
-//        if itemArray[indexPath.row].done == false {
-//            itemArray[indexPath.row].done = true
-//        }else {
-//            itemArray[indexPath.row].done = false
-//        }
-        
         saveItems()
-        
         tableView.deselectRow(at: indexPath, animated: true)
-        
-//        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
-//            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-//        }else{
-//            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-//        }
-//
+
     }
+    
    //MARK - Add New Items
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
         var textField = UITextField()
         let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
-        
         let action = UIAlertAction(title: "Add Item", style: .default){(action) in
             print("Success")
             let newItem = Item()
             newItem.title = textField.text!
             self.itemArray.append(newItem)
-//            self.defaults.set(self.itemArray, forKey: "TodoListArray")
             self.saveItems()
 
         }
@@ -115,6 +61,7 @@ class TodoListViewController: UITableViewController {
         present(alert,animated: true, completion: nil)
 
     }
+    
     //MARK - Model Manipulation Methods
     
     func saveItems() {
@@ -125,7 +72,6 @@ class TodoListViewController: UITableViewController {
         } catch {
             print ("Error encoding item array, \(error)")
         }
-        
         self.tableView.reloadData()
     }
     
